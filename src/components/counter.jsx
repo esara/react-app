cf5aacfc0762c2e7331b202069fb833c1a1aa58b
esara <endresara@gmail.com>
2022-03-23 12:23:24 -0400

    creating navbar

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,31 +1,25 @@
 import React, {Component} from 'react';
 
 class Counter extends Component {
-    state = {
-        value: this.props.counter.value,
-    };
     render() {
         return(
         <div>
             <span className={this.getBadgeClasses.call(this)}> {this.formatCount()}</span>
-            <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm m-2"> Increment</button>
+            <button onClick={() => this.props.onIncrement()} className="btn btn-secondary btn-sm m-2"> Increment</button>
             <button onClick={() => this.props.onDelete()} className="btn btn-danger btn-sm m-2"> Delete</button>
         </div>
         );
     }
 
-    handleIncrement = () => {
-        this.setState({ value: this.state.value + 1 });
-    }
-
     getBadgeClasses() {
         let classes = "badge";
-        classes += (this.state.value === 0) ? "badge-warning" : "badge-primary";
+        classes += (this.props.counter.value === 0) ? "badge-warning" : "badge-primary";
+        classes += "badge-sm m-2";
         return classes;
     }
 
     formatCount() {
-        return this.state.value;
+        return this.props.counter.value === 0 ? 'Zero' : this.props.counter.value;
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counters from "./components/counters";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counters />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
