4e88c4d0916f6bf3fa0564c332a85a46dc7d617a
esara <endresara@gmail.com>
2022-03-23 17:35:00 -0400

    destructuring arguments

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,13 +3,14 @@ import Counter from "./counter";
 
 class Counters extends Component {
     render() {
+        const { counters, onReset, onIncrement, onDelete } = this.props;
         return (
             <div>
-                <button onClick={this.props.onReset} className={"btn btn-primary btn-sm m-2"}> Reset </button>
-                {this.props.counters.map(counter => (
+                <button onClick={onReset} className={"btn btn-primary btn-sm m-2"}> Reset </button>
+                {counters.map(counter => (
                     <Counter key={counter.id }
-                             onIncrement={() => this.props.onIncrement(counter)}
-                             onDelete={() => this.props.onDelete(counter.id)}
+                             onIncrement={() => onIncrement(counter)}
+                             onDelete={() => onDelete(counter.id)}
                              counter={counter}
                              selected={true} />
                 ))}
